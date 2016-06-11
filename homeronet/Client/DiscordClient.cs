7b3026a8e85b8ad5ec9518d4d75c9b626e7a3da5
Darvell Long <contact@darvell.me>
2016-06-10 22:51:02 -0700

    skeletons

--- homeronet/Client/DiscordClient.cs
+++ homeronet/Client/DiscordClient.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using homeronet.EventArgs;
 
 namespace homeronet.Client
 {
-    class DiscordClient
+    public class DiscordClient : IClient
     {
+        public event EventHandler<MessageReceivedEventArgs> ConnectionStatusChanged;
+
+        #region Constructors
+
+        #endregion
+
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Async Methods
+        public Task<bool> Connect()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Methods
+        
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+        
+        #endregion
+        
+        #region Properties
+
+        public bool IsConnected
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IClientConfiguration ClientConfiguration
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+#endregion
     }
 }
--- homeronet/Messages/DiscordMessage.cs
+++ homeronet/Messages/DiscordMessage.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace homeronet.Messages
 {
-    class DiscordMessage
+    public class DiscordMessage
     {
     }
 }
