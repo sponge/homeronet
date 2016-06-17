7f7c8181114e3e3c63736d381d6f7589273526d0
Reece <enemy.forest.brigade@gmail.com>
2016-06-17 19:34:59 +0100

    Reddit commands added

---
+++ homeronet/Plugin/Reddit.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using homeronet.Messages;
+using homeronet.Utility;
+using Newtonsoft.Json.Linq;
+
+namespace homeronet.Plugin
+{
+    public class Reddit : IPlugin
+    {
+        private class RedditCommand
+        {
+            public List<string> Subreddits { get; set; }
+            public Func<string, string> FormatMessage { get; set; }
+        }
+
+        private UriWebClient _webClient;
+        private Dictionary<string, RedditCommand> _commandForName = new Dictionary<string, RedditCommand>()
+        {
+            {
+                "alligator", new RedditCommand()
+                {
+                    Subreddits =  new List<string>() { "britishproblems" },
+                    FormatMessage =  message => "<alligator> " + message.ToLower()
+                }
+            },
+            {
+                "danl", new RedditCommand()
+                {
+                    Subreddits =  new List<string>() { "TheRedPill", "seduction", "CuckoldCommunity", "BikePorn", "electronic_cigarette", "snooker" },
+                    FormatMessage = message => "<bill`gate> " + message.ToLower()
+                }
+            },
+            {
+                "hurt", new RedditCommand()
+                {
+                    Subreddits = new List<string>() { "Buddhism", "explainlikeimfive", "gifs", "Eve", "weightlifting", "MensRights", "hardbodies", "DoesAnybodyElse" },
+                    FormatMessage = message => "<Hurt> " + message
+                }
+            }
+        };
+
+        public void Startup()
+        {
+            _webClient = new UriWebClient();
+        }
+
+        public List<string> RegisteredTextCommands
+        {
+            get { return _commandForName.Keys.ToList(); }
+        }
+
+        public Task<IStandardMessage> ProcessTextCommand(ITextCommand command)
+        {
+            return new Task<IStandardMessage>(() =>
+            {
+                if (_commandForName.ContainsKey(command.Command))
+                {
+                    RedditCommand redditCommand = _commandForName[command.Command];
+                    Random random = new Random();
+
+                    string subreddit = redditCommand.Subreddits[random.Next(redditCommand.Subreddits.Count)];
+
+                    JObject jsonParsed = JObject.Parse(_webClient.DownloadString("http://reddit.com/r/" + subreddit + ".json"));
+                    List<dynamic> postList = jsonParsed["data"]["children"].ToObject<List<dynamic>>();
+
+                    string message = postList[random.Next(postList.Count)]["data"]["title"];
+
+                    return command.InnerMessage.CreateResponse(redditCommand.FormatMessage(message));
+                }
+                return null;
+            });
+        }
+
+        public Task<IStandardMessage> ProcessTextMessage(IStandardMessage message)
+        {
+            return null;
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
