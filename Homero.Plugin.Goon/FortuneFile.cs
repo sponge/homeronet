ba04ba6e302b635371350125daf968809d05ba48
Darvell Long <contact@darvell.me>
2016-06-20 17:01:30 -0700

    work boi

---
+++ Homero.Plugin.Goon/FortuneFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homero.Plugin.Goon
+{
+    internal class FortuneFile
+    {
+        public String Command { get; set; }
+        public String Path { get; set;  }
+        public bool StripNewLines { get; set; }
+        public bool IsMultiLine { get; set; }
+
+        public FortuneFile(string command, string path, bool stripNewLines, bool isMultiline)
+        {
+            Command = command;
+            Path = path;
+            StripNewLines = stripNewLines;
+            IsMultiLine = isMultiline;
+        }
+    }
+}
