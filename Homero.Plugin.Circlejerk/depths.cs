0217c12f089c208b1c745ed3c6d4f13307d6ef15
sponge <sponge@d8d.org>
2016-06-23 10:38:08 -0500

    depths is done for

---
+++ Homero.Plugin.Circlejerk/depths.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Homero.Client;
+using Homero.Messages;
+using Homero.Services;
+
+namespace Homero.Plugin.Circlejerk {
+    public class Depths : IPlugin {
+        private List<string> _registeredCommands = new List<string>() { "depths" };
+
+        public Depths(IMessageBroker broker) {
+            broker.CommandReceived += Broker_CommandReceived;
+        }
+
+        public void Startup() {
+        }
+
+        public void Shutdown() {
+        }
+
+        private void Broker_CommandReceived(object sender, EventArgs.CommandReceivedEventArgs e) {
+            IClient client = sender as IClient;
+
+            var str = String.Join(" ", e.Command.Arguments).Replace('o', 'ø');
+            client?.ReplyTo(e.Command, str);
+        }
+
+        public List<string> RegisteredTextCommands {
+            get { return _registeredCommands; }
+        }
+
+        public Task<IStandardMessage> ProcessTextMessage(IStandardMessage message) {
+            return null;
+        }
+    }
+}
